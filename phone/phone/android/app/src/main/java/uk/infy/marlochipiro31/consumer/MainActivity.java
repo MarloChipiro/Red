0d9d71bf3108d8d0b8155e749caf0b8067a9e378
marlochipiro <chipiromarlo33@gmail.com>
2025-12-11 15:21:37 +0200

    Final CORS and API URL fix for Netlify connection

--- phone/phone/android/app/src/main/java/uk/infy/marlochipiro31/consumer/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package uk.infy.marlochipiro31.consumer;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const JWT_SECRET = 'm1a2r3l4o5; // Change this to a secure, long, random key!
 
 // Middleware setup
 const allowedOrigins = [
-    'https://loquacious-starship-38a2a5.netlify.app/',
+    'https://effulgent-ganache-433653.netlify.app',
     'http://localhost:8888', // Add for local testing with netlify dev
     'http://localhost:3000', // Add if you still want to test on the server's port
 ];
