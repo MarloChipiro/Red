2fea3895bdb4f3b38d52e363356a73adbba2edee
MarloChipiro <chipiromarlo33@gmail.com>
2026-01-27 09:07:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,19 @@
-const CACHE_NAME = 'tracker-v1';
+const CACHE_NAME = 'behavior-tracker-v1';
 const ASSETS = [
   '/',
   '/index.html',
-  '/style.css',
-  'https://cdn.tailwindcss.com',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
